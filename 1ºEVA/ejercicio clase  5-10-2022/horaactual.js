2b34ff8524027805e62690728e3e72d9e7f73fa0
Themorocd <73909321+Themorocd@users.noreply.github.com>
2022-10-05 12:44:45 +0200

    actualizacion
    actualizacion

--- 1ºEVA/ejercicio clase  5-10-2022/horaactual.js
+++ 1ºEVA/ejercicio clase  5-10-2022/horaactual.js
@@ -1,13 +1,30 @@
-let now = new Date();
-
-var dia = now.getDay();
-var mes = now.getMonth();
-var año = now.getFullYear();
-var hora = now.getHours();
-var minutos= now.getMinutes();
-var segundos = now.getSeconds();
 
 
-alert( "fecha: "+`${dia}/${mes}/${año}`+" hora: "+`${hora}/${minutos}/${segundos}`);
+function now(){
+
+    let dia = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+    let mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    let now = new Date();
+
+    if (now.getHours()>12) {
+       horaimprimida=(now.getHours()+":"+now.getMinutes()+":"+now.getSeconds()+" PM"+" "+
+        dia[now.getDay()-1]+" "+now.getDate()+" de "+mes[now.getMonth()]+" de "+now.getFullYear());
+        
+    }else{
+        horaimprimida=(now.getHours()+":"+now.getMinutes()+":"+now.getSeconds()+" AM"+" "+
+        dia[now.getDay()-1]+" "+now.getDate()+" de "+mes[now.getMonth()]+" de "+now.getFullYear());
+    }
+
+        //document.write(dia[now.getDay()-1]+" "+now.getDate()+" de "+mes[now.getMonth()]+" de "+now.getFullYear());*/
+        //setTimeout("now()",1000)
+        document.forms_fecha.fecha.value = horaimprimida;
+        setInterval("now()",1000);
+    
+    }
+    
+    
+        
+        
 
 
