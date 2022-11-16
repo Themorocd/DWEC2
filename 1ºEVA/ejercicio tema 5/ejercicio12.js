d22830784e4a8618bce382e9bfd02831f44e46e9
Themorocd <73909321+Themorocd@users.noreply.github.com>
2022-11-16 12:44:13 +0100

    Update ejercicio12.js

--- 1ºEVA/ejercicio tema 5/ejercicio12.js
+++ 1ºEVA/ejercicio tema 5/ejercicio12.js
@@ -3,6 +3,8 @@ Primero declaro las coordenadas, en este caso x e y
 */
 let x=0;//esta se mueve por left
 let y=0;//esta se mueve por top
+var ancho = window.screen.width;
+var alto = window.screen.height;
 /*
 enganchamos el id del elemento a mover
 */
@@ -15,22 +17,41 @@ function muevo(event){
 el codigo numero de las teclas derecha:39,izq:37,arriba:38,abajo:49
 */
     if(event.keyCode=='39'){//derecha
-        x=x+100;
+        if(x==ancho-100){
+        
+        }else{
+            x=x+10;
         caja.style.left=x+'px';
+        }
+        
     }
     if(event.keyCode=='37'){//izq
-        x=x-100;
-        caja.style.left=x+'px';
+        if(x==0){
+            
+        }else{
+            x=x-10;
+            caja.style.left=x+'px';
+        }
+        
     }
     if(event.keyCode=='38'){//arriba
-        y=y+100;
+        if(y==0){
+            
+        }else{
+            y=y+10;
         caja.style.top=(-y)+'px';
+        }
+        
     }
     if(event.keyCode=='40'){//abajo
-        y=y-100;
-        caja.style.top=(-y)+'px';
+        if(y==alto-240){
+           
+        }else{
+            y=y-10;
+            caja.style.top=(-y)+'px';
+        }
+        
     }
-
 }
 /*
 onkeydown sirve para dectetar la tecla que pulsamos y la comprobamos con las de arriba para mover en la direccion indicada con los if
