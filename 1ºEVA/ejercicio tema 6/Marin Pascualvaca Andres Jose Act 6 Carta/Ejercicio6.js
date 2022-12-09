42ac2da0cdba563006e2593d31b361fb2ce7a636
Themorocd <73909321+Themorocd@users.noreply.github.com>
2022-12-09 12:00:35 +0100

    cambio de colores

--- 1ºEVA/ejercicio tema 6/Marin Pascualvaca Andres Jose Act 6 Carta/Ejercicio6.js
+++ 1ºEVA/ejercicio tema 6/Marin Pascualvaca Andres Jose Act 6 Carta/Ejercicio6.js
@@ -1,4 +1,4 @@
-window.onload = () =>{
+window.onload = () => {
     const motivo = document.forms[0].motivo;//array
     const medalla = document.forms[0].medalla;
     const objetivo = document.forms[0].objetivo;
@@ -6,30 +6,30 @@ window.onload = () =>{
     const carta = document.getElementById('carta');
 
 
-    const cambiocart=()=>{//esto lo que hace es imprimir cada opcion seleccionada en un div que tengo en el html
-        carta.innerHTML=motivotext +medallatext + objetivotext+ despedidatext;
+    const cambiocart = () => {//esto lo que hace es imprimir cada opcion seleccionada en un div que tengo en el html
+        carta.innerHTML = motivotext + medallatext + objetivotext + despedidatext;
 
     }
-   
-    let motivotext= motivo.value;//digo que motivotext es la opcion que he elegido
+
+    let motivotext = motivo.value;//digo que motivotext es la opcion que he elegido
     let medallatext = medalla.value;
     let objetivotext = objetivo.value;
     let despedidatext = despedida.value;
 
     cambiocart();
-    motivo.forEach(x=>x.addEventListener('change',function(){//recorro cada opcion hasta que encuentre la seleccionada y la meto en cambiocart()
+    motivo.forEach(x => x.addEventListener('change', function () {//recorro cada opcion hasta que encuentre la seleccionada y la meto en cambiocart()
         motivotext = this.value;
         cambiocart();
     }));
-    medalla.forEach(x=>x.addEventListener('change',function(){
+    medalla.forEach(x => x.addEventListener('change', function () {
         medallatext = this.value;
         cambiocart();
     }));
-    objetivo.forEach(x=>x.addEventListener('change',function(){
+    objetivo.forEach(x => x.addEventListener('change', function () {
         objetivotext = this.value;
         cambiocart();
     }));
-    despedida.forEach(x=>x.addEventListener('change',function(){
+    despedida.forEach(x => x.addEventListener('change', function () {
         despedidatext = this.value;
         cambiocart();
     }));
